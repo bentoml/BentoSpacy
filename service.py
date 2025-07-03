e3eb47408cdc352bd9acfe276a4ece4319f5033f
Aaron Pham <contact@aarnphm.xyz>
2025-07-03 18:05:02 -0400

    chore: update to latest conventions
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- service.py
+++ service.py
@@ -1,19 +1,21 @@
+from __future__ import annotations
 
 import bentoml
 import spacy
 
-@bentoml.service(traffic={"timeout": 30},
-    resources={
-        "cpu": 1,
-        "memory": "4Gi",
-    },
-)
-class SpaceService:
-    def __init__(self) -> None:
-        self.model = spacy.load("en_core_web_sm")
 
-    @bentoml.api
-    def predict(self, text: str) -> list[list[str, str]]:
-        doc = self.model(text)
-        return [[w.text, w.pos_] for w in doc]
-    
+@bentoml.service(
+  traffic={'timeout': 30},
+  resources={'cpu': 1, 'memory': '4Gi'},
+  image=bentoml.images.Image(python_version='3.11').requirements_file('requirements.txt').uv_lock(),
+)
+class Service:
+  hf = bentoml.models.HuggingFaceModel('spacy/en_core_web_sm')
+
+  def __init__(self):
+    self.model = spacy.load(self.hf)
+
+  @bentoml.api
+  def analyze(self, text: str) -> list[tuple[str, str]]:
+    doc = self.model(text)
+    return [(w.text, w.pos_) for w in doc]
